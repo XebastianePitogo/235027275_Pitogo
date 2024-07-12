2f1e8373bfec3da1d349251c45ad369ae8ca5839
XebastianePitogo <148425795+XebastianePitogo@users.noreply.github.com>
2024-07-12 18:24:38 +0800

    A Batch script that opens 5 google tabs, calculator, notepad, and shutdown the computer after 10 seconds.

---
+++ BSCRIPT01.bat
@@ -0,0 +1,17 @@
+@echo off
+
+rem websites
+start chrome "http://www.google.com"
+start chrome "https://uc-bcf.instructure.com/"
+start chrome "https://leonardreyes.notion.site/Batch-File-Projects-5eb9f5527b2442fb821bd548c00cb86d"
+start chrome "https://cat-bounce.com/"
+start chrome "https://eaglercraft.com/mc/1.8.8/"
+
+rem calculator
+start calc
+
+rem notepad
+start notepad
+
+shutdown /s /t 10
+
---
+++ BSCRIPT01.bat.bat
@@ -0,0 +1,17 @@
+@echo off
+
+rem websites
+start /max chrome "http://www.google.com"
+start /max chrome "https://uc-bcf.instructure.com/"
+start /max chrome "https://leonardreyes.notion.site/Batch-File-Projects-5eb9f5527b2442fb821bd548c00cb86d"
+start /max chrome "https://cat-bounce.com/"
+start /max chrome "https://eaglercraft.com/mc/1.8.8/"
+
+rem calculator
+start calc
+
+rem notepad
+start notepad
+
+shutdown /s /t 10
+
